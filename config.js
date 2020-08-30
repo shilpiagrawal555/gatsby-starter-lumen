aadf1d4e2723d9a44fde7e0e47bf3402faf75c5e
shilpiagrawal555 <shilpiagrawal555@gmail.com>
2020-08-30 22:10:04 +0530

    committing my jekyll theme

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
+  url: 'shilpiagrawal555.github.io',
+  pathPrefix: '/shilpiagrawal555.github.io',
   title: 'Blog by John Doe',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
